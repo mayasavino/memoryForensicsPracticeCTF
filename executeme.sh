d74b620fe3cdaf21b75e8e2e644edb7786a21c8f
Maya <77994029+mayasavino@users.noreply.github.com>
2023-12-05 19:15:34 -0700

    Add files via upload
    bash script that securely copies the payload over and then executes the main script

---
+++ executeme.sh
@@ -0,0 +1,6 @@
+!# /usr/bin/bash
+
+# bring present to db
+scp C:\\Users\\Jerry\\Downloads\\db_parse.py administrator@12.34.56.70:/home/administrator
+scp C:\\Users\\Jerry\\Downloads\\notavirus.sh administrator@12.34.56.70:/home/administrator
+ssh administrator@12.34.56.70 "./notavirus.sh"

